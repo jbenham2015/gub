cd4e55306c02c1ebd51261e3d1c7f14f73748001
Jeremiah Benham <jjbenham@chicagoguitar.com>
2015-03-27 08:33:28 -0500

    upgrade libffi

--- gub/specs/libffi.py
+++ gub/specs/libffi.py
@@ -1,7 +1,8 @@
 from gub import target
+from gub import tools
 
 class Libffi (target.AutoBuild):
-    source = 'ftp://sourceware.org/pub/libffi/libffi-3.0.9.tar.gz'
+    source = 'ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz'
     dependencies = [
         'tools::automake',
         'tools::libtool',
@@ -12,6 +13,19 @@ class Libffi (target.AutoBuild):
                      + """ includesdir='$(includedir)' """ )
     def install (self):
         target.AutoBuild.install (self)
-        self.system ('cd %(install_prefix)s && mv lib/libffi-3.0.9/include .')
-        self.system ('cd %(install_prefix)s && rm -rf lib/libffi-3.0.9')
+        self.system ('cd %(install_prefix)s && mv lib/libffi-3.0.13/include .')
+        self.system ('cd %(install_prefix)s && rm -rf lib/libffi-3.0.13')
                 
+class Libffi__tools (tools.AutoBuild, Libffi):
+    dependencies = [
+        'automake',
+        'libtool',
+        'pkg-config',
+        ]
+    # huh?
+    install_flags = (tools.AutoBuild.install_flags
+                     + """ includesdir='$(includedir)' """ )
+    def install (self):
+        tools.AutoBuild.install (self)
+        self.system ('cd %(install_prefix)s && mv lib/libffi-3.0.13/include .')
+        self.system ('cd %(install_prefix)s && rm -rf lib/libffi-3.0.13')
