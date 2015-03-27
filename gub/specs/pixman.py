62c42ceacf0d26759b79221af788e305ebce0d00
Jeremiah Benham <jjbenham@chicagoguitar.com>
2015-03-27 15:17:48 -0500

    upgrade for pixman

--- gub/specs/pixman.py
+++ gub/specs/pixman.py
@@ -1,8 +1,8 @@
 from gub import target
 
 class Pixman (target.AutoBuild):
-    source = 'http://www.cairographics.org/releases/pixman-0.13.2.tar.gz'
-    dependencies = ['libtool']
+    source = 'http://xorg.freedesktop.org/archive/individual/lib/pixman-0.32.4.tar.bz2'
+    dependencies = ['libtool', 'libpng']
 
 class Pixman__linux__ppc (Pixman):
     patches = ['pixman-0.13.2-auxvec.patch']
