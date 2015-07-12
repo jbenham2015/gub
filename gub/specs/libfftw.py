edcc8622043ffcaa33a9e4cd1c79d69e12c9d8f8
Jeremiah Benham <jjbenham@chicagoguitar.com>
2015-07-12 09:10:41 -0400

    upgrade

--- gub/specs/libfftw.py
+++ gub/specs/libfftw.py
@@ -1,5 +1,5 @@
 from gub import target
 
 class Libfftw (target.AutoBuild):
-    source = 'http://www.fftw.org/fftw-3.2.2.tar.gz'
+    source = 'http://www.fftw.org/fftw-3.3.4.tar.gz'
     dependencies = ['tools::automake', 'tools::libtool', 'tools::pkg-config',]
