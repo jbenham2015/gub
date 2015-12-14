018ce8840c289db67c65eb891c127fdeba1b773d
Jeremiah Benham <jjbenham@chicagoguitar.com>
2015-12-14 00:50:14 -0500

    upgrade

--- gub/specs/fixesproto.py
+++ gub/specs/fixesproto.py
@@ -1,5 +1,5 @@
 from gub import target
 
 class Fixesproto (target.AutoBuild):
-    source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/everything/fixesproto-4.0.tar.gz'
+    source = 'http://pkgs.fedoraproject.org/repo/pkgs/xorg-x11-proto-devel/fixesproto-5.0.tar.bz2/e7431ab84d37b2678af71e29355e101d/fixesproto-5.0.tar.bz2'
     dependencies = ['tools::libtool']
