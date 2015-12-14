67fc1f95f6929efb0930d870fa331e578c4cda34
Jeremiah Benham <jjbenham@chicagoguitar.com>
2015-12-14 00:55:30 -0500

    upgrade

--- gub/specs/libxext.py
+++ gub/specs/libxext.py
@@ -1,7 +1,8 @@
 from gub import target
 
 class Libxext (target.AutoBuild):
-    source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/lib/libXext-1.0.4.tar.gz'
+    source = 'http://ftp.x.org/pub/individual/lib/libXext-1.3.2.tar.bz2'
+#    source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/lib/libXext-1.0.4.tar.gz'
     dependencies = ['libtool', 'libx11-devel']
     configure_flags = (target.AutoBuild.configure_flags
                        + ' --disable-malloc0returnsnull')
